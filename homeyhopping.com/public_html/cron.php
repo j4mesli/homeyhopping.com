e0b0a4a939037821dc7a05c928ef2bbd41525539
j4mesli <34687158+j4mesli@users.noreply.github.com>
2022-10-10 01:58:35 -0400

    updated bin directory of python

--- homeyhopping.com/public_html/cron.php
+++ homeyhopping.com/public_html/cron.php
@@ -1,10 +1,10 @@
 <?php
 $output1 = null;
 $retval = null;
-$command1 = exec('python /spiders/webcrawlerApartmentFinder.py', $output);
-$command2 = exec('python /spiders/webcrawlerApartments.py', $output);
-$command3 = exec('python /spiders/webcrawlerNYRENTOWNSELL.py', $output);
-$command4 = exec('python /update.py', $output);
+$command1 = exec('usr/bin/python /spiders/webcrawlerApartmentFinder.py', $output);
+$command2 = exec('usr/bin/python /spiders/webcrawlerApartments.py', $output);
+$command3 = exec('usr/bin/python /spiders/webcrawlerNYRENTOWNSELL.py', $output);
+$command4 = exec('usr/bin/python /update.py', $output);
 $a = array($command1, $command2, $command3, $command4, $output);
 var_dump($a);
 ?>
