54d6bbf0278975a6d9beab00840a9007aea22bbe
j4mesli <34687158+j4mesli@users.noreply.github.com>
2022-10-10 01:54:04 -0400

    Update cron.php

--- homeyhopping.com/public_html/cron.php
+++ homeyhopping.com/public_html/cron.php
@@ -1,10 +1,10 @@
 <?php
 $output1 = null;
 $retval = null;
-$a = array($command1, $command2, $command3, $command4, $output);
 $command1 = exec('python /spiders/webcrawlerApartmentFinder.py', $output);
 $command2 = exec('python /spiders/webcrawlerApartments.py', $output);
 $command3 = exec('python /spiders/webcrawlerNYRENTOWNSELL.py', $output);
 $command4 = exec('python /update.py', $output);
+$a = array($command1, $command2, $command3, $command4, $output);
 var_dump($a);
 ?>
