8d2f7f26292057372efea1dd08e92c57d9fb5c6a
James Li <34687158+j4mesli@users.noreply.github.com>
2022-11-07 14:59:10 -0500

    added try/catch functionality for cron script

--- homeyhopping.com/public_html/cron.php
+++ homeyhopping.com/public_html/cron.php
@@ -6,6 +6,11 @@ $command2 = exec('usr/bin/python /spiders/webcrawlerApartments.py', $output);
 $command3 = exec('usr/bin/python /spiders/webcrawlerNYRENTOWNSELL.py', $output);
 $command4 = exec('usr/bin/python /update.py', $output);
 $a = array($command1, $command2, $command3, $command4, $output);
-var_dump($a);
-die("UPDATE COMPLETE");
+try {
+  var_dump($a);
+  die("UPDATE COMPLETE");
+}
+catch (Exception e) {
+  die("ERROR: " + e);
+}
 ?>
